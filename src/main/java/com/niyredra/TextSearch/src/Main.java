5999ec32db20746450e36d28b12df40d11995eb7
Astroline <Astroline_kamu@outlook.com>
2022-11-08 14:28:33 +0800

    三数之和 更新了另外的解题思路，但是还未实现

--- src/main/java/com/niyredra/TextSearch/src/Main.java
+++ src/main/java/com/niyredra/TextSearch/src/Main.java
@@ -1,7 +1,16 @@
 package com.niyredra.TextSearch.src;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> list = List.of(4, 5, 6);
+        System.out.println("第1个");
+        //mapMulti产生额外的一个SpinedBuffer对象
+        System.out.println(list.stream()
+                .mapMulti((integer, consumer) -> consumer.accept(List.of(integer)))
+                        .collect(Collectors.toList()));
 
     }
 
